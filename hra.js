1713535a15645bb6ce69251adeb0533d115ee64e
JanaDolejsi <jana.dolejsi.gr@gmail.com>
2024-04-28 10:25:44 +0200

    Úkol č.5 - hotovo

--- hra.js
+++ hra.js
@@ -1,72 +1,71 @@
-import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+import { findWinner } from "https://unpkg.com/piskvorky@0.1.4";
 
-let currentPlayer = "circle"
+let currentPlayer = "circle";
 
 const playerChange = async (event) => {
-    event.target.disabled = true;
-    if (currentPlayer === "circle") {
-        event.target.classList.add("board__field--circle");
-        currentPlayer = "cross";
-        document.getElementById("current__player").src = "cross.svg";
+  event.target.disabled = true;
+  if (currentPlayer === "circle") {
+    event.target.classList.add("board__field--circle");
+    currentPlayer = "cross";
+    document.getElementById("current__player").src = "cross.svg";
+  } else {
+    event.target.classList.add("board__field--cross");
+    currentPlayer = "circle";
+    document.getElementById("current__player").src = "circle.svg";
+  }
+
+  const herniPoleElm = Array.from(buttons);
+  const herniPole = herniPoleElm.map((button) => {
+    if (button.classList.contains("board__field--circle")) {
+      return "o";
+    } else if (button.classList.contains("board__field--cross")) {
+      return "x";
     } else {
-        event.target.classList.add("board__field--cross");
-        currentPlayer = "circle";
-        document.getElementById("current__player").src = "circle.svg";
+      return "_";
     }
-
-    const herniPoleElm = Array.from(buttons);
-    const herniPole = herniPoleElm.map((button) => {
-      if (button.classList.contains("board__field--circle")) {
-         return 'o';
-      } else if (button.classList.contains("board__field--cross")) {
-         return 'x';
-      } else {
-         return '_';
-    }
-    });
-
-    const vitez = findWinner(herniPole);
-
-    if (vitez === 'o' || vitez === 'x') {
-      setTimeout(() => {
-        alert(`Vyhrál hráč se symbolem ${vitez} !`);
-        location.reload();
-      }, 250);
-    } else if (vitez === 'tie') {
-      setTimeout(() => {
-        alert(`Hra skončila remízou!`);
-        location.reload();
-      }, 250)
-    };
-   }; 
-
-const response = await fetch('https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
-   {
-    method:'POST',
-    headers: {'content-type': 'application/json'},
-    body: JSON.stringify({
-      board: herniPole,
-      player: 'x',
-    }),
-  },
-)
-
-const data = await response.json();
-const { x, y } = data.position;
-const novePole = buttons[x + y * 10];
-if (currentPlayer === "cross") {
-  novePole.click();
-}
-
-const buttons = document.querySelectorAll("button")
-buttons.forEach(() => {
-    addEventListener("click",playerChange);
   });
 
+  const vitez = findWinner(herniPole);
 
-document.querySelector(".restart").addEventListener("click",event => {
-    const useRestart = confirm("Ukončit hru?");
-    if (useRestart === false) {
-        event.preventDefault();
+  if (vitez === "o" || vitez === "x") {
+    setTimeout(() => {
+      alert(`Vyhrál hráč se symbolem ${vitez} !`);
+      location.reload();
+    }, 250);
+  } else if (vitez === "tie") {
+    setTimeout(() => {
+      alert(`Hra skončila remízou!`);
+      location.reload();
+    }, 250);
+  }
+  const response = await fetch(
+    "https://piskvorky.czechitas-podklady.cz/api/suggest-next-move",
+    {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        board: herniPole,
+        player: "x",
+      }),
     }
+  );
+
+  const data = await response.json();
+  const { x, y } = data.position;
+  const novePole = buttons[x + y * 10];
+  if (currentPlayer === "cross") {
+    novePole.click();
+  }
+};
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(() => {
+  addEventListener("click", playerChange);
+});
+
+document.querySelector(".restart").addEventListener("click", (event) => {
+  const useRestart = confirm("Ukončit hru?");
+  if (useRestart === false) {
+    event.preventDefault();
+  }
 });
